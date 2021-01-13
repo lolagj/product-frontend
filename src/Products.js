fa86468592f3f84dff9acc1f6e6ef1e82b8ee30f
lola <lolagomezjemes@gmail.com>
2021-01-13 22:42:33 +0000

    Creation of table of products

---
+++ src/Products.js
@@ -0,0 +1,18 @@
+function Products (props){
+    return(
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>&nbsp;</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    )
+}
+
+export default Products;
