db3b8f7e2e96e844871407d13ed2ba2c6844690d
lola <lolagomezjemes@gmail.com>
2021-01-26 10:47:07 +0000

    add support integration with backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+
 import './App.css';
 import Products from './Products.js'
 
 
 function App() {
  
+ 
+    return (
+      <div className="Shopid">
+        
+        <h1>Product Catalogue</h1>
+  
+        <Products/>
+      </div>
+    );
 
-  return (
-    <div className="App">
-      <h1>Product Catalogue</h1>
-      
-      <Products/>
-    </div>
-  );
+ 
+
+  
 }
 
 export default App;
--- src/ProductsApi.js
+++ src/ProductsApi.js
@@ -1,3 +1,4 @@
+const axios= require('axios').default;
 class ProductsApi{
     static API_BASE_URL="/api/v1";
 
@@ -9,7 +10,10 @@ class ProductsApi{
         const headers= this.requestHeaders();
         const request= new Request(ProductsApi.API_BASE_URL+"/products",{
             method: 'GET',
-            headers: headers
+            headers: {
+                headers,
+                apikey: '71c379b0-ca56-4477-a5b8-f9296e5691a0'
+            }
         });
 
         const response = await fetch(request); 
@@ -20,6 +24,30 @@ class ProductsApi{
     
         return response.json();
 
+    }
+
+    static async deleteById(value){
+        axios.delete(ProductsApi.API_BASE_URL + "providers"+ value,
+        {headers:{
+            apikey: '71c379b0-ca56-4477-a5b8-f9296e5691a0'
+        }}).then(res=>{
+            return res;
+        }).cath(error=>{
+            console.error(error)
+            throw Error("Response not valid"+ error);
+        })
+    }
+
+    static async postProduct(value){
+        axios.post(ProductsApi.API_BASE_URL + "providers", value,
+        {headers:{
+            apikey: '71c379b0-ca56-4477-a5b8-f9296e5691a0'
+        }}).then(res=>{
+            return res.data;
+        }).cath(error=>{
+            console.error(error)
+            throw Error("Response not valid"+ error);
+        });
     }
 
 }
