0366ff4138eceb5b55d1bfd32f9b02190e791c1a
lola <lolagomezjemes@gmail.com>
2021-01-27 00:12:36 +0000

    Integration with backend: put, delete, edit

--- src/ProductsApi.js
+++ src/ProductsApi.js
@@ -12,7 +12,7 @@ class ProductsApi{
             method: 'GET',
             headers: {
                 headers,
-                apikey: process.env.BACKEND
+                apikey: process.env.BACKEND || "7144308f-364a-4f26-b480-c72f02ff23a7"
             }
         });
 
@@ -26,6 +26,28 @@ class ProductsApi{
 
     }
 
+    /*
+    static async postProduct(value){
+        const headers= this.requestHeaders();
+        const request= new Request(ProductsApi.API_BASE_URL+"/products",{
+            method: 'POST',
+            headers: {
+                headers,
+                apikey: process.env.BACKEND || "7144308f-364a-4f26-b480-c72f02ff23a7",
+                body: value
+            }
+        });
+
+        const response = await fetch(request); 
+
+        if(! response.ok){
+            throw Error("Response not valid"+ response.status);
+        }
+        
+        console.log(response)
+        return response;
+
+    }*/
 
     static async deleteById(value){
         axios.delete(ProductsApi.API_BASE_URL + "/products/"+ value,
@@ -39,6 +61,7 @@ class ProductsApi{
         })
     }
 
+    
     static async postProduct(value){
         axios.post(ProductsApi.API_BASE_URL + "/products/", value,
         {headers:{
@@ -77,7 +100,7 @@ class ProductsApi{
 
     static async updateProduct(value, body){
         axios.put(ProductsApi.API_BASE_URL+"/products/"+value, body,
-        {headesr:{
+        {headers:{
             apikey: process.env.BACKEND || "7144308f-364a-4f26-b480-c72f02ff23a7"
         }}).then(res=>{
             return res;
